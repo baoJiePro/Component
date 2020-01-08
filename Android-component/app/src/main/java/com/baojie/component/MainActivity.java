4a2e5dbdb486bcc102f51942ac3419c5d06765e8
baojie <baojie@qding.me>
2020-01-08 16:47:52 +0800

    develop分支创建

--- Android-component/app/src/main/java/com/baojie/component/MainActivity.java
+++ Android-component/app/src/main/java/com/baojie/component/MainActivity.java
@@ -1,7 +1,12 @@
 package com.baojie.component;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registerData();
+    }
+
+    private void registerData() {
+        LocalBroadcastManager instance = LocalBroadcastManager.getInstance(this);
+        instance.registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+            }
+        }, new IntentFilter("LOCAL_ACTION"));
     }
 }
